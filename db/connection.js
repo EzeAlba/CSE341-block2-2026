cf4c1d644be9dd69a138c115bad6ffc5bb238086
EzeAlba <ezealba2@gmail.com>
2026-03-06 00:33:32 -0300

    starting project1 first steps

---
+++ db/connection.js
@@ -0,0 +1,38 @@
+
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//const uri = "mongodb+srv://tezting:ezealba2tezt@cse341-cluster.pbppjey.mongodb.net/?appName=CSE341-Cluster";
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Make the appropriate DB calls
+        await listDatabases(client);
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
+run().catch(console.dir);
---
+++ readme.md
@@ -0,0 +1,1 @@
+Project 1 for CSE341 course 
